f7468dd4dae1c5adde3d7a29bb3cfa58019f0ab9
이도현 <cailhuiris@gmail.com>
2017-11-08 13:34:02 +0900

    working with tempaltes

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
-    url(r'^app/', views.index, name = "index")
+    url(r'^app/', views.index, name = "index"),
+    url(r'', views.main, name = "main"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from django.shortcuts import render
 
 # main view
+def main(request):
+    return redirect(index)
+
 def index(request):
     return render(request, 'app/index.html')
