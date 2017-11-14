425366198f8c01a1a08ea05738dec0a0f349ba84
이도현 <cailhuiris@gmail.com>
2017-11-14 17:43:09 +0900

    added feature, update a action.

--- app/static/js/motty.js
+++ app/static/js/motty.js
@@ -10,12 +10,12 @@ app.factory('Actions', ['$resource', function($resource) {
     });
 }]);
 app.factory('Action', ['$resource', function($resource) {
-    return $resource('/app/api/action/:actionId/:action', {actionId:'@id', action:'@action'}, {
+    return $resource('/app/api/action/:id/:action', {id:'@id', action:'@action'}, {
         get: {method:'GET'},
         create: {method:'POST'},
-        update: {method:'POST', action:'edit'},
-        delete: {method:'GET', action:'delete'},
-        deleteAll: {method: 'POST', params: {actionId: 'delete', action: 'all'}}
+        update: {method:'POST', params: { action:'edit'} },
+        delete: {method:'GET', params: {action:'delete'} },
+        deleteAll: {method: 'POST', params: {id: 'delete', action: 'all'}}
     });
 }])
 
@@ -83,7 +83,7 @@ app.controller('ActionList.ctrl', function($scope, $rootScope, $timeout, Actions
 
     $scope.showDetail = function($idx){
         var action = $rootScope.actions[$idx];
-        Action.get({actionId: action.id}, function(res){
+        Action.get({id: action.id}, function(res){
             $scope.action = res;
 
             ngDialog.open({ 
@@ -94,7 +94,6 @@ app.controller('ActionList.ctrl', function($scope, $rootScope, $timeout, Actions
                 onOpenCallback: function(){
                     $timeout(function(){
                         $('pre code').each(function(i, block) {
-                            console.log("block 1");
                             hljs.highlightBlock(block);
                         });
                     }, 100);
@@ -104,6 +103,51 @@ app.controller('ActionList.ctrl', function($scope, $rootScope, $timeout, Actions
     }
 });
 
-app.controller('ActionView.ctrl', function($scope, $timeout){
-    
+app.controller('ActionView.ctrl', function($scope, $timeout, ngDialog){
+    $scope.copyURL = function(){
+        // copy url to clipboard.
+        (function (text) {
+            var node = document.createElement('textarea');
+            var selection = document.getSelection();
+          
+            node.textContent = text;
+            document.body.appendChild(node);
+          
+            selection.removeAllRanges();
+            node.select();
+            document.execCommand('copy');
+          
+            selection.removeAllRanges();
+            document.body.removeChild(node);
+          })('http://localhost:8000/motty/base' + $scope.action.url);
+          
+          $.notify('The full url are copied.', {
+              element: '#view-action-dialog',
+          })
+    }
+
+    $scope.modify = function(){
+        // I used temporarh hidden value due to trouble with ngDialog.
+        $scope.closeThisDialog();
+        $("#update_action_id").val($scope.action.id);
+
+        ngDialog.open({ 
+            template: '/static/templates/create-action-dialog.html', 
+            controller: 'ActionEdit.ctrl'
+        });
+    }
+});
+
+app.controller('ActionEdit.ctrl', function($scope, Action){
+    var id = $("#update_action_id").val();
+    Action.get({id: id}, function(res){
+        $scope.action = res;
+    });
+
+    $scope.save = function(){
+        $scope.closeThisDialog();
+        Action.update($scope.action, function(res){
+            $.notify('Successfully saved.');
+        });
+    };
 });
